2cbd52daa3fddd2604b2e11f65d3f8709527ef46
Eduardo Mendes <efiasn@gmail.com>
2023-05-19 18:39:16 -0300

    Atualização de Arquivos

--- ACS/session/verificaSenha.php
+++ ACS/session/verificaSenha.php
@@ -15,13 +15,13 @@ if (isset($entrar)) {
     if (mysqli_num_rows($verifica)<=0){
         echo"<script language='javascript' type='text/javascript'>
         alert('Login e/ou senha incorretos');window.location
-        .href='login.php';</script>";
+        .href='index.html';</script>";
         die();
     }else{
         echo"<script language='javascript' type='text/javascript'>
         alert('Login correto');</script>";
         setcookie("login",$login);
-        header("Location:../backend.php");
+        header("Location:../agendamentos.php");
       }
     }
 
